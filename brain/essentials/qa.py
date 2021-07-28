1885f2e3e401d6154da5a70fb7e5a0ad2fafa909
arju88nair <arju88nair@gmail.com>
2021-07-28 23:33:23 +0530

    Working QA phase 1

---
+++ brain/essentials/qa.py
@@ -0,0 +1,150 @@
+import torch
+from transformers import BertTokenizer, BertForQuestionAnswering
+import wikipedia as wiki
+import pprint as pp
+
+tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+import textwrap
+
+
+def main():
+    # Wrap text to 80 characters.
+    wrapper = textwrap.TextWrapper(width=80)
+
+    bert_abstract = "Macedonia was an ancient kingdom on the periphery of Archaic and Classical Greece and later the dominant state of Hellenistic Greece. The kingdom was founded and initially ruled by the Argead dynasty, followed by the Antipatrid and Antigonid dynasties. Home to the ancient Macedonians, it originated on the northeastern part of the Greek peninsula. Before the 4th century BC, it was a small kingdom outside of the area dominated by the city-states of Athens, Sparta and Thebes, and briefly subordinate to Achaemenid Persia"
+    question = "Who ruled Macedonia?"
+    question = 'What is the wingspan of an albatross?'
+
+    results = wiki.search(question)
+    print("Wikipedia search results for our question:\n")
+    pp.pprint(results)
+
+    page = wiki.page(results[0])
+    text = page.content
+
+    print(text)
+
+    # answer_question(question, bert_abstract)
+
+
+def tokenize():
+    question = "How many parameters does BERT-large have?"
+    answer_text = "BERT-large is really big... it has 24-layers and an embedding size of 1,024, for a total of 340M parameters! Altogether it is 1.34GB, so expect it to take a couple minutes to download to your Colab instance."
+    # Apply the tokenizer to the input text, treating them as a text-pair.
+    input_ids = tokenizer.encode(question, answer_text)
+    tokens = tokenizer.convert_ids_to_tokens(input_ids)
+
+    print('The input has a total of {:} tokens.'.format(len(input_ids)))
+    # Search the input_ids for the first instance of the `[SEP]` token.
+    sep_index = input_ids.index(tokenizer.sep_token_id)
+
+    # The number of segment A tokens includes the [SEP] token istelf.
+    num_seg_a = sep_index + 1
+
+    # The remainder are segment B.
+    num_seg_b = len(input_ids) - num_seg_a
+
+    # Construct the list of 0s and 1s.
+    segment_ids = [0] * num_seg_a + [1] * num_seg_b
+
+    # There should be a segment_id for every input token.
+    assert len(segment_ids) == len(input_ids)
+    outputs = model(torch.tensor([input_ids]),  # The tokens representing our input text.
+                    token_type_ids=torch.tensor([segment_ids]),
+                    # The segment IDs to differentiate question from answer_text
+                    return_dict=True)
+
+    start_scores = outputs.start_logits
+    end_scores = outputs.end_logits
+    # Find the tokens with the highest `start` and `end` scores.
+    answer_start = torch.argmax(start_scores)
+    answer_end = torch.argmax(end_scores)
+
+    # Combine the tokens in the answer and print it out.
+    answer = ' '.join(tokens[answer_start:answer_end + 1])
+
+    print('Answer: "' + answer + '"')
+    # Start with the first token.
+    answer = tokens[answer_start]
+
+    # Select the remaining answer tokens and join them with whitespace.
+    for i in range(answer_start + 1, answer_end + 1):
+
+        # If it's a subword token, then recombine it with the previous token.
+        if tokens[i][0:2] == '##':
+            answer += tokens[i][2:]
+
+        # Otherwise, add a space then the token.
+        else:
+            answer += ' ' + tokens[i]
+
+    print('Answer: "' + answer + '"')
+
+
+def answer_question(question, answer_text):
+    '''
+    Takes a `question` string and an `answer_text` string (which contains the
+    answer), and identifies the words within the `answer_text` that are the
+    answer. Prints them out.
+    '''
+    # ======== Tokenize ========
+    # Apply the tokenizer to the input text, treating them as a text-pair.
+    input_ids = tokenizer.encode(question, answer_text)
+
+    # Report how long the input sequence is.
+    print('Query has {:,} tokens.\n'.format(len(input_ids)))
+
+    # ======== Set Segment IDs ========
+    # Search the input_ids for the first instance of the `[SEP]` token.
+    sep_index = input_ids.index(tokenizer.sep_token_id)
+
+    # The number of segment A tokens includes the [SEP] token istelf.
+    num_seg_a = sep_index + 1
+
+    # The remainder are segment B.
+    num_seg_b = len(input_ids) - num_seg_a
+
+    # Construct the list of 0s and 1s.
+    segment_ids = [0] * num_seg_a + [1] * num_seg_b
+
+    # There should be a segment_id for every input token.
+    assert len(segment_ids) == len(input_ids)
+
+    # ======== Evaluate ========
+    # Run our example through the model.
+    outputs = model(torch.tensor([input_ids]),  # The tokens representing our input text.
+                    token_type_ids=torch.tensor([segment_ids]),
+                    # The segment IDs to differentiate question from answer_text
+                    return_dict=True)
+
+    start_scores = outputs.start_logits
+    end_scores = outputs.end_logits
+
+    # ======== Reconstruct Answer ========
+    # Find the tokens with the highest `start` and `end` scores.
+    answer_start = torch.argmax(start_scores)
+    answer_end = torch.argmax(end_scores)
+
+    # Get the string versions of the input tokens.
+    tokens = tokenizer.convert_ids_to_tokens(input_ids)
+
+    # Start with the first token.
+    answer = tokens[answer_start]
+
+    # Select the remaining answer tokens and join them with whitespace.
+    for i in range(answer_start + 1, answer_end + 1):
+
+        # If it's a subword token, then recombine it with the previous token.
+        if tokens[i][0:2] == '##':
+            answer += tokens[i][2:]
+
+        # Otherwise, add a space then the token.
+        else:
+            answer += ' ' + tokens[i]
+
+    print('Answer: "' + answer + '"')
+
+
+if __name__ == '__main__':
+    main()
