66e2788f11e14c87a57ec9be550bac618935e43a
arju88nair <arju88nair@gmail.com>
2021-10-17 21:01:21 +0530

    Extracting notes

--- src/brain/skills/collection/notes.py
+++ src/brain/skills/collection/notes.py
@@ -1,8 +1,18 @@
 from brain.skills.skill import AssistantSkill
+import re
 
 
 class NoteSkill(AssistantSkill):
     @classmethod
-    def blah(cls, voice_transcript, **kwargs):
+    def blah(cls, voice_transcript, skill):
+        tags = cls.extract_tags(voice_transcript, skill['tags'])
         print(voice_transcript)
-        return "Fd"
+        print(tags)
+        only_text_pattern = '([a-zA-Z]+)'
+        for tag in tags:
+            reg_ex = re.search(tag + ' ' + only_text_pattern, voice_transcript)
+            print(reg_ex)
+            print("sda")
+            if reg_ex:
+                topic = reg_ex.group(1)
+                print(topic)
